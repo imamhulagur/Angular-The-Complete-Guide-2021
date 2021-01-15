0744711aa63643ad372a671c883dec926aec7c8c
i721901 <imamsab.hulagur@voya.com>
2021-01-15 12:20:40 +0530

    Done - Directives

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule } from '@angular/forms';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FormsModule } from '@angular/forms';
     ShoppingListComponent,
     RecipeDetailsComponent,
     ShoppingEditComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
